7c17fd28d0c4ff45498717febea1c774666cac79
Joanna Fengler <asiapobuta@wp.pl>
2018-03-17 05:32:06 +0100

    make code readable

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,12 @@ var triangle1Area = getTriangleArea(10, 15),
 	triangle2Area = getTriangleArea(2, 3),
 	triangle3Area = getTriangleArea(5, 6);
 
-function getTriangleArea(a, h) {
-if (a <= 0 || h <= 0) { 
-	console.log('niewłaściwe dane');
-} else {
-	return console.log(a * h / 2);
-	}
-}
-console.log(getTriangleArea(10, 6))
+	function getTriangleArea(a, h) {
+		if (a <= 0 || h <= 0) { 
+		console.log('niewłaściwe dane');
+		} else {
+			return console.log(a * h / 2);
+			}
+		}
+		
+	console.log(getTriangleArea(10, 6))
