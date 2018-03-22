8726c9dd9894247c8a4827e94796f0f72692ee11
Joanna Fengler <asiapobuta@wp.pl>
2018-03-22 22:12:54 +0100

    display triangleArea

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,18 @@
-// Hello.
-//
-// This is a task "what if".
-
-/*alert var name = prompt('Enter your name');
-('Hello, ' + name);
-console.log('Hello, ' + name);*/
 
 var triangle1Area = getTriangleArea(10, 15),																		
 	triangle2Area = getTriangleArea(2, 3),
 	triangle3Area = getTriangleArea(5, 6);
 
-	function getTriangleArea(a, h) {
-		if (a <= 0 || h <= 0) { 
-		console.log('niewłaściwe dane');
-		} else {
-			return console.log(a * h / 2);
-			}
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
+
+function getTriangleArea(a, h) {
+	if (a <= 0 || h <= 0) { 
+	return 'niewłaściwe dane';
+	} else {
+		return(a * h / 2);
 		}
+	};
 		
-	console.log(getTriangleArea(10, 6))
+console.log(getTriangleArea(10, 6));
